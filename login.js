7ab35e327d3d76920dda8d1a71062be8dcf8ad20
Matt Rentaro <hsudoru@gmail.com>
2024-07-09 21:39:40 +0800

    aa

---
+++ login.js
@@ -0,0 +1,16 @@
+function trackFunction() {
+    var username = document.getElementById('fname').value;
+    var password = document.getElementById('lname').value;
+
+    console.log("Username:", username);
+    console.log("Password:", password);
+
+    // Simple username and password check
+    if (username === "user" && password === "pass") {
+        localStorage.setItem('isLoggedIn', 'true');
+        return true; // Allow form submission
+    } else {
+        alert("Invalid credentials. Please contact site admin.");
+        return false; // Prevent form submission
+    }
+}
